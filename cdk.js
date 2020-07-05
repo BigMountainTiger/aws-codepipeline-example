6b080fc4a4e4f9911b17adaa62d872089b3baf8f
Song Li <da_tou_li@yahoo.com>
2020-07-05 17:02:41 -0400

    Renamed some files

--- cdk.js
+++ cdk.js
@@ -1,7 +1,10 @@
 #!/usr/bin/env node
 
 const cdk = require('@aws-cdk/core');
-const { AwsCodepipelineExampleStack } = require('./stacks/aws-codepipeline-example-stack');
+const { AwsCodepipelineBucketStack } = require('./stacks/aws-codepipeline-bucket-stack/aws-codepipeline-bucket-stack');
 
 const app = new cdk.App();
-new AwsCodepipelineExampleStack(app, 'AwsCodepipelineExampleStack');
+const AWS_CODEPIPELINE_BUCKET_STACK_NAME = 'AWS-CODEPIPELINE-BUCKET-STACK';
+new AwsCodepipelineBucketStack(app, AWS_CODEPIPELINE_BUCKET_STACK_NAME, {
+  description: AWS_CODEPIPELINE_BUCKET_STACK_NAME
+});
---
+++ stacks/aws-codepipeline-bucket-stack/aws-codepipeline-bucket-stack.js
@@ -0,0 +1,11 @@
+const cdk = require('@aws-cdk/core');
+
+class AwsCodepipelineBucketStack extends cdk.Stack {
+
+  constructor(scope, id, props) {
+    super(scope, id, props);
+
+  }
+}
+
+module.exports = { AwsCodepipelineBucketStack }
