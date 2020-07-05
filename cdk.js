f30fa8e9730476192a37b90987865ce650df26bc
Song Li <da_tou_li@yahoo.com>
2020-07-05 17:42:55 -0400

    Added the pipeline stack template

--- cdk.js
+++ cdk.js
@@ -2,9 +2,17 @@
 
 const cdk = require('@aws-cdk/core');
 const { AwsCodepipelineBucketStack } = require('./stacks/aws-codepipeline-bucket-stack/aws-codepipeline-bucket-stack');
+const { AwsCodepipelinetStack } = require('./stacks/aws-codepipeline-stack/aws-codepipeline-stack');
 
 const app = new cdk.App();
+
+
 const AWS_CODEPIPELINE_BUCKET_STACK_NAME = 'AWS-CODEPIPELINE-BUCKET-STACK';
 new AwsCodepipelineBucketStack(app, AWS_CODEPIPELINE_BUCKET_STACK_NAME, {
   description: AWS_CODEPIPELINE_BUCKET_STACK_NAME
+});
+
+const AWS_CODEPIPELINE_STACK_NAME = 'AWS-CODEPIPELINE-STACK';
+new AwsCodepipelinetStack(app, AWS_CODEPIPELINE_STACK_NAME, {
+  description: AWS_CODEPIPELINE_STACK_NAME
 });
---
+++ stacks/aws-codepipeline-stack/aws-codepipeline-stack.js
@@ -0,0 +1,11 @@
+const cdk = require('@aws-cdk/core');
+
+class AwsCodepipelinetStack extends cdk.Stack {
+
+  constructor(scope, id, props) {
+    super(scope, id, props);
+
+  }
+}
+
+module.exports = { AwsCodepipelinetStack }
