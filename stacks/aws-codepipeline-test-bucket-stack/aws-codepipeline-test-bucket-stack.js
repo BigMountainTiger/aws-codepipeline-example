0797c82a46231cb0df169b0226d2967d007a17dd
Song Li <da_tou_li@yahoo.com>
2020-07-06 14:51:07 -0400

    Remove a bucket

--- stacks/aws-codepipeline-test-bucket-stack/aws-codepipeline-test-bucket-stack.js
+++ stacks/aws-codepipeline-test-bucket-stack/aws-codepipeline-test-bucket-stack.js
@@ -13,14 +13,6 @@ class AwsCodepipelineTestBucketStack extends cdk.Stack {
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       lifecycleRules: [ {expiration: cdk.Duration.days(1)} ]
     });
-
-    bucket_name = `huge-head-li-test-bucket-1`;
-    new s3.Bucket(this, bucket_name, {
-      bucketName: bucket_name,
-      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
-      removalPolicy: cdk.RemovalPolicy.DESTROY,
-      lifecycleRules: [ {expiration: cdk.Duration.days(1)} ]
-    });
   }
 }
 
