3404b3ae11134e9ccf989cf0512f4cef3c31c831
Song Li <da_tou_li@yahoo.com>
2020-07-06 15:33:04 -0400

    Added the test lambda stack

--- cdk.js
+++ cdk.js
@@ -4,6 +4,7 @@ const cdk = require('@aws-cdk/core');
 const addPipelineStacks = require('./pipeline/add-pipeline-stacks');
 
 const { AwsCodepipelineTestBucketStack } = require('./stacks/aws-codepipeline-test-bucket-stack/aws-codepipeline-test-bucket-stack');
+const { AwsCodepipelineTestLambdaStack } = require('./stacks/aws-codepipeline-test-lambda-stack/aws-codepipeline-test-lambda-stack');
 
 const app = new cdk.App();
 
@@ -12,4 +13,9 @@ addPipelineStacks.add(app);
 const AWS_CODEPIPELINE_TEST_BUCKET_STACK_NAME = 'AWS-CODEPIPELINE-TEST-BUCKET-STACK';
 new AwsCodepipelineTestBucketStack(app, AWS_CODEPIPELINE_TEST_BUCKET_STACK_NAME, {
   description: AWS_CODEPIPELINE_TEST_BUCKET_STACK_NAME
+});
+
+const AWS_CODEPIPELINE_TEST_LAMBDA_STACK_NAME = 'AWS-CODEPIPELINE-TEST-LAMBDA-STACK';
+new AwsCodepipelineTestLambdaStack(app, AWS_CODEPIPELINE_TEST_LAMBDA_STACK_NAME, {
+  description: AWS_CODEPIPELINE_TEST_LAMBDA_STACK_NAME
 });
